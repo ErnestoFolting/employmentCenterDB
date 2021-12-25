7a2c16b8426abd7ae5452ce960bd34c40faf39d8
Danil <danilsmy.edu@gmail.com>
2021-12-25 16:59:42 +0200

    Add 2 procedures

--- procedures.sql
+++ procedures.sql
@@ -15,3 +15,35 @@ end
 
 exec findDeals '+380956078557'
 
+--2. Процедура, що показує усіх підібраних для певної компанії можливих робітників, по номеру телефона компанії
+create proc findDealsForCompany
+	@companyPhoneNumberToFind nvarchar(13)
+as
+begin
+	select PIB, expectedSalary, allJoblesses.position as expectedPosition ,workExperience, allJoblesses.educationTypeName, allJoblesses.phoneNumber from vacancy
+	join deal on deal.vacancyId = vacancy.id
+	join company on company.id = vacancy.companyId
+	join allJoblesses on allJoblesses.id = deal.joblessId
+	where company.id = (
+		select companyId 
+		from companyPhoneNumbers 
+		where phoneNumber = @companyPhoneNumberToFind
+	)
+	
+end
+
+exec findDealsForCompany '+380636056449'
+
+--3. Процедура що дозволяє агенту додавати угоди до списку угод та показати оновлений список угод
+create proc addDeal
+	@joblessToAdd int,
+	@vacancyToAdd int,
+	@agentWhoAdds int
+as
+begin
+	insert into deal(agentId,vacancyId,joblessId)
+	values(@agentWhoAdds,@vacancyToAdd,@joblessToAdd)
+	select * from allDeals
+end
+
+exec addDeal 2,29,2
